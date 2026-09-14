d53940688175941fe90140f2769f95fa0e33af91
nailaahmad128-source <nailaahmad128@gmail.com>
2026-09-14 18:14:20 +0500

    Fix PowerPoint conversion compatibility

--- lib/core/services/conversion/local_office_converter.dart
+++ lib/core/services/conversion/local_office_converter.dart
@@ -644,7 +644,6 @@ class LocalOfficeConverter implements ConversionProvider {
           width: width,
           height: height,
           fontSize: _safeFontSize(line.fontSize),
-          italic: _isItalic(line.fontStyle),
         );
       }
 
